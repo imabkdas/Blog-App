1213ef33e8c2e28fddc4e9d9ea5f73c14fffb850
imabkdas <anand.abhishek269@gmail.com>
2020-09-23 13:33:43 +0530

    sd

--- routes/index.js
+++ routes/index.js
@@ -98,7 +98,7 @@ router.post('/forgot', function(req, res, next) {
         subject: 'Blog Post password reset',
         text: 'You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n' +
           'Please click on the following link, or paste this into your browser to complete the process:\n\n' +
-          'http://' + req.headers.host + '/reset/' + token + '\n\n' +
+          'http://' + 'obscure-springs-49161.herokuapp.com' + '/reset/' + token + '\n\n' +
           'If you did not request this, please ignore this email and your password will remain unchanged.\n'
       };
       smtpTransport.sendMail(mailOptions, function(err) {
