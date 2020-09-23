69e23720fb76f465e4bdbf10eea774811ad61ffe
imabkdas <anand.abhishek269@gmail.com>
2020-09-23 12:35:09 +0530

    jfgej

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
-var dotenv			= require('dotenv').config();
+if(process.env.NODE_ENV !== 'production') {
+	require('dotenv').config()
+}
+
 var express 		= require("express"),
 		app 		= express(),
  bodyParser 		= require("body-parser"),
